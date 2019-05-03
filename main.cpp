a0eaf189614f3561355e7d6fc1e9ef42bda299af
SB <2sbsbsb@gmail.com>
2019-05-03 02:40:09 -0700

    cleanup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  udacity_cpp_nanodegree
+//
+//  Created by Santosh Bhushan on 5/3/19.
+//  Copyright © 2019 sb. All rights reserved.
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  udacity_cpp_nanodegree
+//
+//  Created by Santosh Bhushan on 5/3/19.
+//  Copyright © 2019 sb. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
